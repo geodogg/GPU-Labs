b936d7588c0e2b39c4acae7dad3605c4710e13c3
geodogg <hojoduurenbileg@gmail.com>
2018-06-04 16:17:00 -0600

    helloworldgpu

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -13,22 +13,21 @@ int main(int argc, char * argv[]){
     printf("Running program: %s\n", argv[0]);
     printf("Hello! Welcome to the HelloWorld equivalent of CUDA.\n");
 
-    // ~~~~~~~~~~~~~~~vector addition~~~~~~~~~~~~~~~
-
-    // allocate memory on HOST
-
     int N = 1 << 20;  // approximately a million elements
 
+    // ~~~~~~~~~~~~~~~ vector addition - HOST ~~~~~~~~~~~~~~~
+
+    // generate vectors for addition
     float * c = new float[N]; // allocate memory of million floats on HOST
     if ( c == NULL ) exit (1);  // error check
-
-    // generate vectors to add
-    float * a = new float[N];
+    float * a = new float[N]; // allocate memory on HOST
     if ( a == NULL ) exit (1);  // error check
-    float * b = new float[N];
+    float * b = new float[N]; // allocate memory on HOST
     if ( b == NULL ) exit (1);  // error check
 
-    printf ("Number of bytes allocated: %d bytes\n", N);
+    printf ("Number of bytes allocated per vector: %d bytes\n", N);
+
+    printf("~~~~~~Vector addition on HOST~~~~~~\n");
 
     // initialize a and b on HOST
     for (int i = 0; i < N; i++){
@@ -36,18 +35,20 @@ int main(int argc, char * argv[]){
       b[i] = 2.0f;
     }
 
-    clock_t tic;
+    clock_t tic = clock();  // start clocking
 
-    tic = clock();
-    add(N, c, a, b);
-    printf("Addition of a[0] b[0] equals %f.\n", c[0]);
+    add(N, c, a, b);  // vector addition on HOST
+
+    printf("Addition of a[0] b[0] equals %f.\n", c[0]); // print single element output
 
     clock_t toc = clock() - tic;
     float elapsed_time = ((float)toc) / CLOCKS_PER_SEC;
 
     printf("Vector addition on the HOST\nElapsed time: %f (sec)\n", elapsed_time);
 
-    // run vector addition on HOST
+    // ~~~~~~~~~~~~~~~ vector addition - DEVICE ~~~~~~~~~~~~~~~
+    printf("~~~~~~Vector addition on DEVICE~~~~~~\n");
+
 
 
     return 0;
