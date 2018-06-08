2d1d45657f207752cfb0eca5a427033b018d53bf
geodogg <hojoduurenbileg@gmail.com>
2018-06-08 09:32:48 -0600

    cuda2

--- HelloWorld/HelloWorldCUDA/functions.cu
+++ HelloWorld/HelloWorldCUDA/functions.cu
@@ -13,9 +13,8 @@ void KernelAdd(int n, float * p_sum, float * x, float * y){
   return;
 }
 
-void DeviceKernel(int numBlocks, int sizeBlock, int n, float * p_sum, float * x, float * y, string kernel_name){
+void DeviceKernel(int numBlocks, int sizeBlock, int n, float * p_sum, float * x, float * y){
 
-  if (kernel_name == "add"){
     KernelAdd <<<numBlocks, sizeBlock>>> (N, d_c, d_a, d_b);  // vector addition on DEVICE
-  }
+
 }
--- HelloWorld/HelloWorldCUDA/functions.cuh
+++ HelloWorld/HelloWorldCUDA/functions.cuh
@@ -1,8 +1,7 @@
 #ifndef FUNCTIONS_CUH
 #define FUNCTIONS_CUH
 
-#include <string>
 
-void DeviceKernel(int numBlocks, int sizeBlock, int n, float * p_sum, float * x, float * y, string kernel_name);
+void DeviceKernel(int numBlocks, int sizeBlock, int n, float * p_sum, float * x, float * y);
 
 #endif
