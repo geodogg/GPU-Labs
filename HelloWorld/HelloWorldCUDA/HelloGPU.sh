16cacd91e560f1aa405ffe37d1962efbf9baf3d4
geodogg <hojoduurenbileg@gmail.com>
2018-06-11 13:47:20 -0600

    sd

--- HelloWorld/HelloWorldCUDA/HelloGPU.sh
+++ HelloWorld/HelloWorldCUDA/HelloGPU.sh
@@ -2,6 +2,6 @@
 #SBATCH --account=def-spichard
 #SBATCH --gres=gpu:1              # request GPU "generic resource", 4 on Cedar, 2 on Graham
 #SBATCH --mem=16GB               # memory per node
-#SBATCH --time=0-00:1            # time (DD-HH:MM) haha
+#SBATCH --time=0-00:2            # time (DD-HH:MM) haha
 ./hello
 ./deviceQuery
