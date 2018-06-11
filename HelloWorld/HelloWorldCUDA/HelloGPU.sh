9845f45a2710ede334ca010e131427daecfa618f
geodogg <hojoduurenbileg@gmail.com>
2018-06-11 11:34:21 -0600

    s

--- HelloWorld/HelloWorldCUDA/HelloGPU.sh
+++ HelloWorld/HelloWorldCUDA/HelloGPU.sh
@@ -2,6 +2,6 @@
 #SBATCH --account=def-spichard
 #SBATCH --gres=gpu:1              # request GPU "generic resource", 4 on Cedar, 2 on Graham
 #SBATCH --mem=16GB               # memory per node
-#SBATCH --time=0-00:10            # time (DD-HH:MM) haha
+#SBATCH --time=0-00:1            # time (DD-HH:MM) haha
 ./hello
 -I/cvmfs/soft.computecanada.ca/easybuild/software/2017/avx2/Compiler/intel2016.4/cuda/9.0.176/extras/demo_suite ./deviceQuery
