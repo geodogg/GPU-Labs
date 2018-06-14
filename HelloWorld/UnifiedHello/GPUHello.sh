828f478e4501c9904bf24728e7b1402b9f6743f5
geodogg <hojoduurenbileg@gmail.com>
2018-06-14 13:59:42 -0600

    unifiedmemory

---
+++ HelloWorld/UnifiedHello/GPUHello.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#SBATCH --account=def-spichard
+#SBATCH --gres=gpu:1              # request GPU "generic resource", 4 on Cedar, 2 on Graham
+#SBATCH --mem=256MB               # memory per node
+#SBATCH --time=0-00:2            # time (DD-HH:MM) hahahaha
+./hello
---
+++ HelloWorld/UnifiedHello/hello.cu
@@ -0,0 +1,87 @@
+#include <cstdio>
+#include <cassert>  // useful for debugging
+#include <cstdlib>
+#include <ctime>
+
+// cuda library inlcludes
+#include <cuda_runtime.h>
+#include <helper_cuda.h>
+#include <cooperative_groups.h>
+#include "functions.cuh"
+
+using namespace std;
+
+// GPU error checking macro
+#define gpuErrchk(ans){ gpuAssert((ans), __FILE__, __LINE__); }
+inline void gpuAssert(cudaError_t code, const char * file, int line, bool abort = true){
+  if (code != cudaSuccess){
+    fprintf(stderr, "gpuAssert: %s %s %d\n", cudaGetErrorString(code), file, line);
+    exit(code);
+  }
+}
+
+// outputs debugging macro
+#define printline(ans) { fprintf(outfile, "file: %s line: %d\n - ", __FILE__, __LINE__); fprintf(outfile, ans); }
+
+// initialize unified memory
+int N = 1 << 10;  // 1024 elements
+__device__ __managed__ float c[N];
+
+// addition kernel
+__global__
+void KernelAdd(int N, float a, float b){
+
+  // int i = blockIdx.x * blockDim.x + threadIdx.x;
+  // if ( i < N )
+  //   c[i] = a + b + i;
+
+  // using single block
+  c[threadIdx.x] = a + b + threadIdx.x;
+}
+
+int main(int argc, char * argv[]){
+    printf("Running program: %s\n", argv[0]);
+
+    // outfile for debugging
+    FILE * outfile;
+    outfile = fopen("debug.txt", "w");
+    if (outfile == NULL){
+      printf(".....there is an error opening debug file....\n");
+      return 0;
+    }
+
+    printline("Hello! Welcome to the HelloWorld equivalent of CUDA.\n")
+
+    float a = 1.0;
+    float b = 2.0;
+
+    int sizeBlock = 256;
+    int numBlocks = 1;
+//  scanf("Please enter number of threads per block:%i\n", sizeBlock);
+
+    clock_t tic = clock();  // start clocking
+
+    printline("Check\n")
+
+    KernelAdd<<< numBlocks, sizeBlock >>> (N, a, b);
+
+    printline("Check\n")
+
+    // Wait for GPU to finish before accessing on host
+//    cudaDeviceSynchronize();
+    // waiting for Device to synchronize on cedar or busy remote servers will cause
+    // program to take forever.
+
+    for(auto iter = c.begin(); iter != c.end(); iter++)
+      fprintf(outfile, "c[%d] = %f", i, c[i]);
+
+    printline("Check\n")
+
+    clock_t toc = clock() - tic;
+    float elapsed_time = ((float)toc) / CLOCKS_PER_SEC;
+
+    printf("Vector addition on the DEVICE\nElapsed time: %f (sec)\n", elapsed_time);
+
+    fclose(outfile);
+    return 0;
+}
