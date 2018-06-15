ef40d00f190f9b42e5a14d01bc3193f8f6a2ec0e
geodogg <hojoduurenbileg@gmail.com>
2018-06-15 15:04:33 -0600

    s

--- HelloWorld/UnifiedHello/hello.cu
+++ HelloWorld/UnifiedHello/hello.cu
@@ -29,14 +29,14 @@ __device__ __managed__ float c[ (int) 1 << 20];
 __global__
 void KernelAdd(int N, float a, float b){
 
-  int index = blockIdx.x * blockDim.x + threadIdx.x;
-  int stride = blockDim.x * gridDim.x;
-  for (int i = index; i < N; i += stride)
-      c[i] = a + b + i;
+  // int index = blockIdx.x * blockDim.x + threadIdx.x;
+  // int stride = blockDim.x * gridDim.x;
+  // for (int i = index; i < N; i += stride)
+  //     c[i] = a + b + i;
 
-  // int i = blockIdx.x * blockDim.x + threadIdx.x;
-  // if ( i < N )
-  //   c[i] = a + b + i;
+  int i = blockIdx.x * blockDim.x + threadIdx.x;
+  if ( i < N )
+    c[i] = a + b + i;
 
   // using single block
   // c[threadIdx.x] = a + b + threadIdx.x;
@@ -61,18 +61,18 @@ int main(int argc, char * argv[]){
     float b = 0.01;
 
     int sizeBlock = 1024;
-    int numBlocks = 1;
+    int numBlocks = 1024;
 //  scanf("Please enter number of threads per block:%i\n", sizeBlock);
 
     clock_t tic = clock();  // start clocking
 
-    clock_t toc = clock() - tic;
-    float elapsed_time = ((float)toc) / CLOCKS_PER_SEC;
-
     printline("Check\n")
 
     KernelAdd<<< numBlocks, sizeBlock >>> (N, a, b);
-    cudaDeviceSynchronize();
+//    cudaDeviceSynchronize();
+
+    clock_t toc = clock() - tic;
+    float elapsed_time = ((float)toc) / CLOCKS_PER_SEC;   // finish clocking
 
     printline("Check\n")
 
