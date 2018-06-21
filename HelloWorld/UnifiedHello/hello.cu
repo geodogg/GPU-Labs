28989b52ca52325219dbff03b9524acd3698688c
geodogg <hojoduurenbileg@gmail.com>
2018-06-21 14:12:45 -0600

    ad

--- HelloWorld/UnifiedHello/hello.cu
+++ HelloWorld/UnifiedHello/hello.cu
@@ -23,12 +23,20 @@ inline void gpuAssert(cudaError_t code, const char * file, int line, bool abort 
 // outputs debugging macro
 #define printline(ans) { fprintf(outfile, "file: %s line: %d\n - ", __FILE__, __LINE__); fprintf(outfile, ans); }
 
+
+#define N (1 << 20)
+#define sizeBlock (1024)
+#define numBlocks (1024)
+
 // initialize unified memory
 __device__ __managed__ float c[ (int) 1 << 20];
 
 // addition kernel
 __global__
-void KernelAdd(int N, float a, float b){
+void KernelAdd(){
+
+  __shared__ float a = 0.10;
+  __shared__ float b = 0.01;
 
   int index = blockIdx.x * blockDim.x + threadIdx.x;
   int stride = blockDim.x * gridDim.x;
@@ -43,12 +51,14 @@ void KernelAdd(int N, float a, float b){
 
 // subtraction kernel
 __global__
-void KernelSub(int N, float a, float b){
+void KernelSub(){
 
+  __shared__ float a = 0.10;
+  __shared__ float b = 0.01;
   int index = blockIdx.x * blockDim.x + threadIdx.x;
   int stride = blockDim.x * gridDim.x;
   for (int i = index; i < N; i += stride)
-      c[i] = a - b - i;
+      c[i] = c[i] - a - b - i;
 
   // int i = blockIdx.x * blockDim.x + threadIdx.x;
   // if ( i < N )
@@ -69,22 +79,19 @@ int main(int argc, char * argv[]){
 
     printline("Hello! Welcome to the HelloWorld equivalent of CUDA.\n")
 
-    int N = 1 << 20;  // 1024 elements
-
-    float a = 0.10;
-    float b = 0.01;
-
-    int sizeBlock = 1024;
-    int numBlocks = 1024;
 //  scanf("Please enter number of threads per block:%i\n", sizeBlock);
 
     clock_t tic = clock();  // start clocking
 
     printline("Check\n")
 
-    cudaSetDevice(0);
+    int p = 0;
+    for(int i = 0; i < N; p++, i+=(1<<p))
+      fprintf(outfile, "c[%d] = %f\n", i, c[i]);
 
-    KernelAdd<<< numBlocks, sizeBlock >>> (N, a, b);
+    cudaSetDevice(1);
+
+    KernelAdd<<< numBlocks, sizeBlock >>> ();
     cudaDeviceSynchronize();
 
     clock_t toc = clock() - tic;
@@ -94,15 +101,22 @@ int main(int argc, char * argv[]){
 
     printf("Vector addition on the DEVICE 0\nElapsed time: %f (sec)\n", elapsed_time);
 
-
     printline("Check\n")
-    int p = 0;
+    p = 0;
     for(int i = 0; i < N; p++, i+=(1<<p))
       fprintf(outfile, "c[%d] = %f\n", i, c[i]);
 
+    cudaSetDevice(0);
 
-    cudaSetDevice(1);
+    tic = clock();  // start clocking
+
+    cudaDeviceSynchronize();
     KernelSub<<< numBlocks, sizeBlock >>> (N, a, b);
+
+    toc = clock() - tic;
+    elapsed_time = ((float)toc) / CLOCKS_PER_SEC;   // finish clocking
+
+    printf("Vector addition on the DEVICE 0\nElapsed time: %f (sec)\n", elapsed_time);
 
     printf("Vector subtration on the DEVICE 1\n");
 
