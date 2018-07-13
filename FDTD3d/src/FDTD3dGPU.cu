a146934251aa27a8986e99f4af6c51dd327e50a9
geodogg <hojoduurenbileg@gmail.com>
2018-07-12 18:42:58 -0600

    asdf

--- FDTD3d/src/FDTD3dGPU.cu
+++ FDTD3d/src/FDTD3dGPU.cu
@@ -255,7 +255,7 @@ bool fdtdGPU(cudaStream_t *streams, DEVICES *arr_device, float *output, const fl
     checkCudaErrors(cudaDeviceSynchronize());
 
     // Read the result back, result is in bufferSrc (after final toggle)
-    checkCudaErrors(cudaMemcpy((void *)output, bufferSrc, volumeSize * sizeof(float), cudaMemcpyHostToDevice));
+    checkCudaErrors(cudaMemcpy(output, bufferSrc, volumeSize * sizeof(float), cudaMemcpyDeviceToHost));
 
     // Report time
 #ifdef GPU_PROFILING
