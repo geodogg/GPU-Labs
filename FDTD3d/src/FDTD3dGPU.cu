d57924b6f12593159e20d4f3195af89bc359b62b
geodogg <hojoduurenbileg@gmail.com>
2018-07-15 13:22:05 -0600

    asd

--- FDTD3d/src/FDTD3dGPU.cu
+++ FDTD3d/src/FDTD3dGPU.cu
@@ -48,7 +48,7 @@ bool fdtdGPU(cudaStream_t *streams, DEVICES *arr_device, float *output, const fl
     const int         outerDimx  = dimx + 2 * radius;
     const int         outerDimy  = dimy + 2 * radius;
     const int         outerDimz  = dimz + 2 * radius;
-    const size_t      volumeSize = outerDimx * outerDimy * outerDimz);
+    const size_t      volumeSize = outerDimx * outerDimy * outerDimz;
     // int               deviceCount  = 0;
     // int               targetDevice = 0;
     // float            *bufferOut    = 0;
