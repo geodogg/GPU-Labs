893de3d01c854f141a1ffe7765843bcd77c153c2
geodogg <hojoduurenbileg@gmail.com>
2018-07-24 14:23:04 -0600

    sad

--- FDTD3d/inc/FDTD3dGPUKernel.cuh
+++ FDTD3d/inc/FDTD3dGPUKernel.cuh
@@ -11,7 +11,6 @@
 
  #include "FDTD3dGPU.h"
  #include <cooperative_groups.h>
- #include <cuda_driver.h>
 
  namespace cg = cooperative_groups;
 
@@ -40,7 +39,18 @@
      cg::thread_block cta = cg::this_thread_block();
      __shared__ float tile[k_blockDimMaxY + 2 * RADIUS][k_blockDimX + 2 * RADIUS];
 
-     cuPrintf("dimx: %d  dimy: %d\n", dimx, dimy);
+     printf("dimx: %d  dimy: %d\n", dimx, dimy);
+     printf("dimx: %d  dimy: %d\n", dimx, dimy);
+     printf("dimx: %d  dimy: %d\n", dimx, dimy);
+     printf("dimx: %d  dimy: %d\n", dimx, dimy);
+     printf("dimx: %d  dimy: %d\n", dimx, dimy);
+     printf("dimx: %d  dimy: %d\n", dimx, dimy);
+     printf("dimx: %d  dimy: %d\n", dimx, dimy);
+     printf("dimx: %d  dimy: %d\n", dimx, dimy);
+     printf("dimx: %d  dimy: %d\n", dimx, dimy);
+     printf("dimx: %d  dimy: %d\n", dimx, dimy);
+     printf("dimx: %d  dimy: %d\n", dimx, dimy);
+     printf("dimx: %d  dimy: %d\n", dimx, dimy);
 
      int num_d = arr_device[0].num_devices;
      int current_device = 0;
