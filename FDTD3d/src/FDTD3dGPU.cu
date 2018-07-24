a07b77d8faa55dd6607848dcd657e37ca38d8bf5
geodogg <hojoduurenbileg@gmail.com>
2018-07-24 16:02:58 -0600

    ads

--- FDTD3d/src/FDTD3dGPU.cu
+++ FDTD3d/src/FDTD3dGPU.cu
@@ -193,7 +193,7 @@ bool fdtdGPU(cudaStream_t *streams, DEVICES *arr_device, float *output, const fl
 
             checkCudaErrors(cudaSetDevice(arr_device[i].device));
 
-            FiniteDifferencesKernel<<<arr_device[i].dimGrid, arr_device[i].dimBlock, 960 * sizeof(float), streams[i]>>>(arr_device[i].d_out, bufferDst, arr_device[i].d_in, dimx, dimy / arr_device[0].num_devices, dimz, arr_device);
+            FiniteDifferencesKernel<<<arr_device[i].dimGrid, arr_device[i].dimBlock>>>(arr_device[i].d_out, bufferDst, arr_device[i].d_in, dimx, dimy / arr_device[0].num_devices, dimz, arr_device);
             cudaDeviceSynchronize();
 
             float *tmp = arr_device[i].d_out;
@@ -201,7 +201,7 @@ bool fdtdGPU(cudaStream_t *streams, DEVICES *arr_device, float *output, const fl
             arr_device[i].d_in = tmp;
 
         }
-
+// , 960 * sizeof(float), streams[i]
         // Toggle the buffers
         // Visual Studio 2005 does not like std::swap
         //    std::swap<float *>(bufferSrc, bufferDst);
