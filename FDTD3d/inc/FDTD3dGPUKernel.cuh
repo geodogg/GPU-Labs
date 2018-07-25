3fde26ed230d7890c74cf7f1aeb10cdcae4f4f34
geodogg <hojoduurenbileg@gmail.com>
2018-07-25 15:34:13 -0600

    ads

--- FDTD3d/inc/FDTD3dGPUKernel.cuh
+++ FDTD3d/inc/FDTD3dGPUKernel.cuh
@@ -190,7 +190,7 @@ __global__ void FiniteDifferencesKernel(float *output,
 
    int num_d = arr_device[0].num_devices;
    int current_device = 0;
-//   cudaGetDevice(&current_device);
+   cudaGetDevice(&current_device);
    const int gpu_place = arr_device[current_device].gpu_case;
 
    int inputIndex  = 0;
