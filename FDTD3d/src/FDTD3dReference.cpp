953384ff1c4aa7194d9ef710d0c195ac50f66517
geodogg <hojoduurenbileg@gmail.com>
2018-07-25 17:02:37 -0600

    asd

--- FDTD3d/src/FDTD3dReference.cpp
+++ FDTD3d/src/FDTD3dReference.cpp
@@ -157,8 +157,6 @@ bool compareData(const float *output, const float *reference, const float *input
                         printf("reference: %f;   output: %f;   input: %f\n", *reference, *output, *input);
                         counter++;
                     }
-                    else
-                        return true;
 
                     // Determine the relative error
                     if (*reference != 0)
