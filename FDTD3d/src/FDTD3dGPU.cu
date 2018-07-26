d06596a9f986433a4f325682b6d9cb08490e7f49
geodogg <hojoduurenbileg@gmail.com>
2018-07-26 14:06:50 -0600

    sad

--- FDTD3d/src/FDTD3dGPU.cu
+++ FDTD3d/src/FDTD3dGPU.cu
@@ -202,7 +202,7 @@ bool fdtdGPU(cudaStream_t *streams, DEVICES *arr_device, float *output, const fl
 
             FiniteDifferencesKernel<<<arr_device[i].dimGrid, arr_device[i].dimBlock, 0, streams[i]>>>(arr_device[i].d_out, bufferDst, arr_device[i].d_in, dimx, dimy / arr_device[0].num_devices, dimz, arr_device, arr_device[i].device);
 
-            checkCudaErrors(cudaDeviceSynchronize());
+//            checkCudaErrors(cudaDeviceSynchronize());
             dim3 dimblock;
             dim3 dimgrid;
             dimblock.x = 1;
