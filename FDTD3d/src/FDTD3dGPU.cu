84fed0de1ea9b142d93c924a8c1cbf3ba8a91beb
geodogg <hojoduurenbileg@gmail.com>
2018-07-27 14:20:55 -0600

    s

--- FDTD3d/src/FDTD3dGPU.cu
+++ FDTD3d/src/FDTD3dGPU.cu
@@ -217,7 +217,7 @@ bool fdtdGPU(cudaStream_t *streams, DEVICES *arr_device, float *output, const fl
             //
             // checkCudaErrors(cudaDeviceSynchronize());
             //
-            // compareDataSmall<<<dimgrid, dimblock>>>(arr_device[i].d_out, arr_device[i].d_in, dimx, dimy / arr_device[0].num_devices, dimz, radius, 0.000100);
+            compareDataSmall<<<dimgrid, dimblock>>>(arr_device[i].d_out, arr_device[i].d_in, dimx, dimy / arr_device[0].num_devices, dimz, radius, 0.000100);
 
             float *tmp = arr_device[i].d_out;
             arr_device[i].d_out = arr_device[i].d_in;
